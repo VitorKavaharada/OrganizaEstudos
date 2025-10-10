40a3d7b1ac3042ab5e6e2942e31f6412cc253aa5
Vitor <vkavaharada@gmail.com>
2025-10-10 09:58:54 -0300

    feat: adiciona o StudyController e seus métodos

---
+++ controllers/StudyController.js
@@ -0,0 +1,73 @@
+
+const Study = require('../models/Study');
+
+module.exports = class StudyController {
+
+  static createStudy(req, res) {
+
+    res.render('studies/create');
+
+  }
+
+  static async createStudySave(req, res) {
+
+    const study = {
+      subject: req.body.subject,
+      notes: req.body.notes,
+      completed: false
+    };
+
+    await Study.create(study);
+    res.redirect('/studies');
+  }
+
+  static async removeStudy(req, res) {
+
+    const id = req.body.id;
+    await Study.destroy({ where: { id: id } });
+    res.redirect('/studies');
+
+  }
+
+  static async updateStudy(req, res) {
+
+    const id = req.params.id;
+    const study = await Study.findOne({ where: { id: id }, raw: true });
+    res.render('studies/edit', { study });
+
+  }
+
+  static async updateStudyPost(req, res) {
+
+    const id = req.body.id;
+
+    const study = {
+      subject: req.body.subject,
+      notes: req.body.notes
+    };
+
+    await Study.update(study, { where: { id: id } });
+    res.redirect('/studies');
+
+  }
+
+  static async toggleStudyStatus(req, res) {
+
+    const id = req.body.id;
+
+    const study = {
+      completed: req.body.completed === '0' ? true : false
+    };
+
+    await Study.update(study, { where: { id: id } });
+    res.redirect('/studies');
+
+  }
+
+  static async index(req, res) {
+
+    const studies = await Study.findAll({ raw: true });
+    res.render('studies/all', { studies });
+  }
+  
+};
