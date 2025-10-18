88679f9abb011df5d1138733ad8db63e0534c58f
Vitor <vkavaharada@gmail.com>
2025-10-18 16:35:33 -0300

    feat: adiciona as rotas para recuperar e redefinir senha

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,9 @@ router.get('/register', AuthController.showRegister);
 router.post('/register', AuthController.register);
 router.get('/logout', AuthController.logout);
 
+router.get('/forgot-password', AuthController.showForgotPassword);
+router.post('/forgot-password', AuthController.forgotPassword);
+router.get('/reset-password', AuthController.showResetPassword);
+router.post('/reset-password', AuthController.resetPassword);
+
 module.exports = router;
